1fc04459ac23c5c1e7db3df8112c359f2da3ae73
Zi Huijie <zi.huijie@gmail.com>
2017-09-22 20:52:12 -0600

    added mongodb

--- src/index.js
+++ src/index.js
@@ -3,15 +3,27 @@ import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import schema from './schema';
 
+import connectMongo from './mongo-connector';
+
+const start = async () => {
+const mongo = await connectMongo();
+
 const app = express();
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
-app.use('/graphiql', graphiqlExpress({
-  endpointURL:'/graphql',
-}));
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+  context: {mongo},
+  schema }));
 
-const PORT = 3000;
+  app.use('/graphiql', graphiqlExpress({
+    endpointURL:'/graphql',
+  }));
 
-app.listen(PORT, () => {
-  console.log(`App is running on port ${PORT}`);
-})
+
+  const PORT = 3000;
+
+  app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+  })
+
+}
+start();
---
+++ src/mongo-connector.js
@@ -0,0 +1,8 @@
+import { MongoClient } from 'mongodb';
+
+const MONGO_URL = 'mongodb://localhost:27017/hackernews';
+
+export default async () => {
+  const db = await MongoClient.connect(MONGO_URL);
+  return { Links: db.collection('links')};
+}
